66d26a1e82cfaa776050417641f47533b80eafe4
Safin Ali <safin.ali.7205@gmail.com>
2023-01-25 23:51:55 +0600

    declared product brands name

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { additionalImgsReducer } from './features/aditional-imagesSlice/additionalImagesSlice';
 import { categoriesReducer } from './features/categorySlice/categorySlice';
+import { prodBrandsReducer } from './features/product-brands/prodBrandsSlice';
 
 export const store = configureStore({
     reducer: {
         categories: categoriesReducer,
         addtionalImgs: additionalImgsReducer,
+        productBrands:prodBrandsReducer
     }
 })
