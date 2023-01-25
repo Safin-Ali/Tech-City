ab4e90b4e72df06292d40d91aae7ff9f3917849d
Safin Ali <safin.ali.7205@gmail.com>
2023-01-25 23:55:50 +0600

    implement center prop for rendering loading component window center

--- src/Components/Spinner/LoadingSpinner.js
+++ src/Components/Spinner/LoadingSpinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './spinner.css';
 
-const LoadingSpinner = ({center,middle}) => {
+const LoadingSpinner = ({center,middle,blurBG}) => {
     return (
-        <div className={`${center && 'flex justify-center'} ${middle && 'items-center'}`}>
+        <div className={`${center && 'flex justify-center'} ${middle && 'items-center'} ${blurBG && 'blur'}`}>
             <span className="loader"></span>
         </div>
     );
