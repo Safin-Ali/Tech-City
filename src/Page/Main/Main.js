465771f34a1658a386c305966fa901b1547bc403
Safin Ali <safin.ali.7205@gmail.com>
2023-01-26 00:21:37 +0600

    properly fixed mobile responsive

--- src/Page/Main/Main.js
+++ src/Page/Main/Main.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchAdditionalImgs } from '../../app/features/aditional-imagesSlice/additionalImagesSlice';
 import Navbar from '../../Components/Navbar/Navbar';
 import useFetch from '../../Hooks/useFetch';
 import {Outlet, useLocation} from 'react-router-dom';
 import autoTitleChanger from '../../Hooks/autoTitleChanger';
 import { fetchCategories } from '../../app/features/categorySlice/categorySlice';
+import { fetchProdBrands } from '../../app/features/product-brands/prodBrandsSlice';
 
 const Main = () => {
 
@@ -14,16 +15,26 @@ const Main = () => {
     //auto fetch for categories
     useFetch(fetchCategories);
 
+    //auto fetch for productBrands
+    useFetch(fetchProdBrands);
+
     // get pathName of URL
     const pathName = useLocation().pathname;
 
     // auto change the web title
-    autoTitleChanger(pathName)
+    autoTitleChanger(pathName);
+
+
+
+    // get header section height
+    const [headerSecH,setHeaderSecH] = useState(0);
+
+    useEffect(()=>setHeaderSecH(document.querySelector('header').clientHeight+'px'),[]);
 
     return (
         <>
             <Navbar></Navbar>
-            <main className={`max-h-[90vh] overflow-y-scroll`}>
+            <main style={{height: `calc(100vh - ${headerSecH})`}} className={`overflow-y-scroll`}>
                 <Outlet></Outlet>
             </main>
         </>
