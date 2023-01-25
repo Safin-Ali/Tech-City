01c8ca82503cdcd06d4a4ac02d0baf6325c94ef4
Safin Ali <safin.ali.7205@gmail.com>
2023-01-26 00:02:58 +0600

    dynamicly create form input element from fetchesSchemaValue if schema value null form showing no data found avatar

--- src/Components/Form/ProductAddition.js
+++ src/Components/Form/ProductAddition.js
@@ -1,73 +1,99 @@
-import React, { useEffect, useState } from 'react';
+import React,{useState } from 'react';
 import handleDynaForm from '../../lib/dyna-form/handleDynaForm';
 import axios from 'axios';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useLocation } from 'react-router-dom';
 import ScaleFeild from '../InputFeild/ScaleFeild';
 import FileUploadFeild from '../InputFeild/FileUploadFeild';
 import PrimaryButton from '../Button/PrimaryButton';
+import { useSelector } from 'react-redux';
+import SelectFeild from '../InputFeild/SelectFeild';
+import { MobileSchema } from '../../Schema/products/mobile/mobile';
+import EmptyData from '../Error/EmptyData';
 
 const ProductAddition = () => {
+
+    const [storeBrand,setBrand] = useState(null);
+
     const handleForm = (obj,clearForm) => {
-        console.log(obj)
+        const mobileSchema = new MobileSchema ({...obj,brand: storeBrand});
+        console.log(mobileSchema)
     };
+
+    const location = useLocation().pathname.split('/')[2];
+
+    // get all brandName
+    const {prodBrands} = useSelector(state => state.productBrands);
+
+    // filter brandName
+    const filteredBrands = prodBrands.map(obj => {
+       const lowerCaseStr = obj.product.map(str => str.toLowerCase());
+       return {...obj, product: lowerCaseStr};
+    }).filter( elm => elm.product.includes(location));
 
     // get loaded schema data
     const fetchedData = useLoaderData();
 
+    if(!fetchedData) return <EmptyData></EmptyData>
+
     return (
-    <>
-        <section className={`m-[5%]`}>
-            <form onSubmit={handleDynaForm(handleForm)}>
-                <div className={`grid grid-cols-1 sm:grid-cols-2 gap-5 justify-center`}>
-                    {
-                        Object.keys(fetchedData).filter(filterElm => filterElm !== '_id' && filterElm !== 'device').map((elm,id) => {
-                            const exactValue = fetchedData[elm];
+        <>
+            <section className={`m-[5%]`}>
+                <form onSubmit={handleDynaForm(handleForm)}>
+                    <div className={`grid grid-cols-1 sm:grid-cols-2 gap-5 justify-center`}>
+                        {
+                            Object.keys(fetchedData).filter(filterElm => filterElm !== '_id' && filterElm !== 'device').map((elm,id) => {
+                                const exactValue = fetchedData[elm];
 
-                            return elm === 'ProductImages' ?
+                                return elm === 'ProductImages' ?
 
-                            <div key={id} className={`col-span-1 w-full sm:col-span-2 sm:w-2/3 md:w-1/2 mx-auto`}> <FileUploadFeild name={elm}></FileUploadFeild></div> :
+                                <div key={id} className={`col-span-1 w-full sm:col-span-2 sm:w-2/3 md:w-1/2 mx-auto`}> <FileUploadFeild name={elm}></FileUploadFeild></div> :
 
-                            <div key={id} className={`border p-5`}>
-                                <h4 className={`my-2`}>{elm}</h4>
-                                    {
-                                        Array.isArray(exactValue) ?
+                                <div key={id} className={`border p-5 rounded-md hover:shadow hover:shadow-blackAlpha`}>
+                                    <h4 className={`my-2`}>{elm}</h4>
+                                        {
+                                            Array.isArray(exactValue) ?
 
-                                        exactValue.map((childElm,childId) => {
-                                            const childElmName = Object.keys(childElm)[0];
-                                            const childElmArray = childElm[childElmName];
-                                            const checkChildArrType = Array.isArray(childElmArray);
-                                            return <div key={childId}>
-                                                <div className={`${checkChildArrType ? 'flex gap-5 items-center' : ''}`}>
+                                            exactValue.map((childElm,childId) => {
+                                                const childElmName = Object.keys(childElm)[0];
+                                                const childElmArray = childElm[childElmName];
+                                                const checkChildArrType = Array.isArray(childElmArray);
+                                                return <div key={childId}>
+                                                    <div className={`${checkChildArrType ? 'flex gap-5 items-center' : ''} my-3`}>
 
-                                                {checkChildArrType ?
+                                                    {checkChildArrType ?
 
-                                                <div>
-                                                    <h5>{childElmName}</h5></div> :
+                                                    <div>
+                                                        <h5>{childElmName}</h5></div> : childElmName === 'Brand' ?
 
-                                                <ScaleFeild value={childElmName}></ScaleFeild>}
+                                                        <>
+                                                        <h5>Brand</h5>
+                                                        <SelectFeild setValCallback={setBrand} options={filteredBrands}></SelectFeild>
+                                                        </> :
 
-                                                    {
-                                                        checkChildArrType && childElmArray.map((iiChild,iiChildId) => <div key={iiChildId}>
-                                                            <ScaleFeild value={iiChild}></ScaleFeild>
-                                                        </div>)
-                                                    }
+                                                        <ScaleFeild name={`${childElmName+elm}`} value={childElmName}></ScaleFeild>}
 
+                                                        {
+                                                            checkChildArrType && childElmArray.map((iiChild,iiChildId) => <div key={iiChildId}>
+                                                                <ScaleFeild name={childElmName+iiChild} value={iiChild}></ScaleFeild>
+                                                            </div>)
+                                                        }
+
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        }) :
+                                            }) :
 
-                                        <ScaleFeild value={exactValue}></ScaleFeild>
-                                    }
+                                            <ScaleFeild name={`${elm+exactValue}`} value={exactValue}></ScaleFeild>
+                                        }
 
-                            </div>
-                        })
-                    }
-                </div>
+                                </div>
+                            })
+                        }
+                    </div>
 
-                <div className={`text-center my-5`}><PrimaryButton padding={'px-8 py-2'}>POST</PrimaryButton></div>
-            </form>
-        </section>
-    </>
+                    <div className={`text-center my-5`}><PrimaryButton padding={'px-8 py-2'}>POST</PrimaryButton></div>
+                </form>
+            </section>
+        </>
     );
 };
 
