0b691b50ea6350030f0aabc8a6d73754293ef64a
Safin Ali <safin.ali.7205@gmail.com>
2023-02-03 01:37:27 +0600

    added product list page and fetch product by params

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -3,6 +3,7 @@ import ProductAddition from '../Components/Form/ProductAddition';
 import Development from '../Page/Development/Development';
 import Home from '../Page/Home/Home';
 import Main from '../Page/Main/Main';
+import ProductItems from '../Page/Product-Items/ProductItems';
 
 export const router = createBrowserRouter([
     {
@@ -15,6 +16,9 @@ export const router = createBrowserRouter([
             },
             {
                 path: `/development/:device`, loader: async ({params}) => fetch (`https://tech-city.vercel.app/caregorySchema/${params.device}`) , element: <ProductAddition></ProductAddition> , errorElement: <p>Network Error</p>
+            },
+            {
+                path: `/products/:device`, loader: async ({params}) => fetch (`https://tech-city.vercel.app/products/${params.device}`) , element: <ProductItems></ProductItems> , errorElement: <p>Network Error</p>
             },
         ]
     },
