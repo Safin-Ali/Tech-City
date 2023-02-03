c0dad558d4279af4943cf9cd930b1cabddd95536
Safin Ali <safin.ali.7205@gmail.com>
2023-02-03 22:43:44 +0600

    Sidebar Component Created

---
+++ src/Components/Navbar/LeftBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftBar = () => {
+    return (
+        <aside className={`border-r`}>
+            left
+        </aside>
+    );
+};
+
+export default LeftBar;
---
+++ src/Components/Navbar/RightBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightBar = () => {
+    return (
+        <aside className={`border-l`}>
+            right
+        </aside>
+    );
+};
+
+export default RightBar;
