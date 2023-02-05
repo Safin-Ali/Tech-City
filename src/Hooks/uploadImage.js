e903818da664e2a2776bbe2043615f8542f2ae67
Safin Ali <safin.ali.7205@gmail.com>
2023-02-06 00:42:29 +0600

    image uploading validtion bugs fixed

--- src/Hooks/uploadImage.js
+++ src/Hooks/uploadImage.js
@@ -2,32 +2,41 @@ import axios from "axios";
 
 const checkImgSize = async (file) => {
     const fileType = file?.type.split("/")[0];
-    if(fileType !== "image") return alert('Please Select Only Image File');
+    if (fileType !== "image") return alert('Please Select Only Image File');
     const reader = new FileReader();
     reader.readAsDataURL(file);
     return new Promise(resolve => {
         reader.onload = function () {
-            const img = new Image();
+            const img = new Image;
             img.src = reader.result;
-             img.onload = function () {
-                if(img.width && img.height !== 500) return resolve(false);
-                resolve(img.width)
+            img.onload = function () {
+
+                if (img.width !== img.height) {
+                    alert('Sorry! Image Width & Height Not Equal');
+                    return resolve(false);
+                } else if (img.width !== 500) {
+                    alert('Sorry! Image Width Not Match');
+                    resolve(img.width);
+                } else if (img.height !== 500) {
+                    return resolve(false);
+                } else {
+                    return resolve(true);
+                }
             };
-        };
+        }
     });
-}
+};
 
 const uploadImage = async (files) => {
-    try{
-
+    try {
         const imageSize = await checkImgSize(files);
-        if(!imageSize) return alert('Sorry! Image Size Max');
+        if (!imageSize) return false;
         const imageData = new FormData;
-        imageData.append('image',files)
-        const res =  await axios.post(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMGBB_TECH_CITY_SECRET}`,imageData);
+        imageData.append('image', files)
+        const res = await axios.post(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMGBB_TECH_CITY_SECRET}`, imageData);
         return res.data.data.url;
     }
-    catch(e){
+    catch (e) {
         return console.log(e.message)
     }
 };
