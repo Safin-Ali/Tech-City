5c04e11a62a00e139ff311858626f3c0fe6b0079
Safin Ali <safin.ali.7205@gmail.com>
2023-02-06 00:40:11 +0600

    capitialize all category name

--- src/Components/InputFeild/SelectFeild.js
+++ src/Components/InputFeild/SelectFeild.js
@@ -4,11 +4,11 @@ const SelectFeild = ({options,setValCallback}) => {
 
     const handleSelectedValue = (e) => {
         return setValCallback(e.target.value || null)
-    }
+    };
 
     return (
         <div className={`my-3`}>
-            <select id="countries" defaultValue={'null'} onChange={handleSelectedValue} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <select id="countries" defaultValue={'null'} onChange={handleSelectedValue} className="bg-gray-50 capitalize border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                 <option value='null' disabled={true}>Choose Brand</option>
                 {
                     options?.map( elm => <option key={elm._id} value={elm.brandName}>{elm.brandName}</option>)
