3f274676a96ee2060070aa2cff01732077d7e3a2
Safin Ali <safin.ali.7205@gmail.com>
2023-02-05 15:51:30 +0600

    parseInt product price

--- src/Schema/products/mobile/mobile.js
+++ src/Schema/products/mobile/mobile.js
@@ -1,63 +1,63 @@
 export class MobileSchema {
-    constructor (obj) {
-    this.performance  = {
-        RAM: obj['RamRam & ROM'] + ' GB',
-        ROM: obj['ROMRam & ROM'] + ' GB',
-        CPUName: obj.CPUName,
-        CPUModel: obj.CPUModel,
-        GPUName: obj.GPUName,
-        GPUModel: obj.GPUModel,
-    };
+    constructor(obj) {
+        this.performance = {
+            RAM: obj['RamRam & ROM'] + ' GB',
+            ROM: obj['ROMRam & ROM'] + ' GB',
+            CPUName: obj.CPUName,
+            CPUModel: obj.CPUModel,
+            GPUName: obj.GPUName,
+            GPUModel: obj.GPUModel,
+        };
 
-    this.camera = {
-        front: obj.FrontCamera + ' px',
-        rear: obj.RearCamera + ' px'
-    };
+        this.camera = {
+            front: obj.FrontCamera + ' px',
+            rear: obj.RearCamera + ' px'
+        };
 
-    this.capacity  = {
-        bettery: obj['BetteryCharge & Bettery'] + ' mAh',
-        charger: obj['ChargerCharge & Bettery'] + 'W'
-    };
+        this.capacity = {
+            bettery: obj['BetteryCharge & Bettery'] + ' mAh',
+            charger: obj['ChargerCharge & Bettery'] + 'W'
+        };
 
-    this.touchScreen = {
-        size: obj.SizeDisplay + ' Inch',
-        technology: obj.TechnologyDisplay + ' Touchscreen',
-        resolution: obj.ResolutionDisplay,
+        this.touchScreen = {
+            size: obj.SizeDisplay + ' Inch',
+            technology: obj.TechnologyDisplay + ' Touchscreen',
+            resolution: obj.ResolutionDisplay,
 
-    };
+        };
 
-    this.connectivity = {
-        network: obj.NetworkConnection + ' VoLTE',
-        wifi: obj.WifiConnection + 'G',
-        usb: obj.USBConnection
-    };
+        this.connectivity = {
+            network: obj.NetworkConnection + ' VoLTE',
+            wifi: obj.WifiConnection + 'G',
+            usb: obj.USBConnection
+        };
 
-    this.system = {
-        android: obj.OSSystem,
-        sensors: obj.SensorsSystem,
-    };
+        this.system = {
+            android: obj.OSSystem,
+            sensors: obj.SensorsSystem,
+        };
 
-    this.security = obj.SecuritySystem.split(',');
+        this.security = obj.SecuritySystem.split(',');
 
-    this.price = {
-        base: obj.BasePrice,
-        deivery: obj.DeliveryPrice,
-        discount: obj.DiscountPrice|| 0,
-        total: Math.round(parseInt(obj.BasePrice) - (parseInt(obj.BasePrice) * parseInt(obj.DiscountPrice)/100)) + obj.DeliveryPrice
-    };
+        this.price = {
+            base: obj.BasePrice,
+            deivery: obj.DeliveryPrice,
+            discount: obj.DiscountPrice || 0,
+            total: Math.round(parseInt(obj.BasePrice) - (parseInt(obj.BasePrice) * parseInt(obj.DiscountPrice) / 100)) + obj.DeliveryPrice
+        };
 
-    this.others = {
-        year: obj.YearOthers,
-        color: obj.ColorOthers,
-        model: obj.ModelOthers,
-    };
+        this.others = {
+            year: obj.YearOthers,
+            color: obj.ColorOthers,
+            model: obj.ModelOthers,
+        };
 
-    this.brand = obj.brand;
+        this.brand = obj.brand.toLowerCase();
 
-    this.device = obj.device;
+        this.device = obj.device;
 
-    this.activity = obj.activity;
+        this.activity = obj.activity;
 
-    this.deviceImage = obj.ProductImages[0];
+        this.deviceImage = obj.ProductImages[0];
     };
 };
