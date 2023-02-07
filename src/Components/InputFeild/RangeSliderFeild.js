b1359619759b80cb657af4b82e25f2e52009d940
Safin Ali <safin.ali.7205@gmail.com>
2023-02-08 01:16:39 +0600

    margin removed

--- src/Components/InputFeild/RangeSliderFeild.js
+++ src/Components/InputFeild/RangeSliderFeild.js
@@ -10,9 +10,8 @@ const RangeSliderFeild = () => {
     }
 
     return (
-        <div className={`relative my-5 p-4`}>
-            <label htmlFor="steps-range" className="block font-medium text-gray-900">Price Range</label>
-            <input onInput={handleSlider} id="steps-range" type="range" step="100" min="100" defaultValue={0} max="500" className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" />
+        <div className={`relative p-4`}>
+            <input onInput={handleSlider} id="steps-range" type="range" step="100" min="100" defaultValue={0} max="500" className="w-full h-2 bg-white rounded-lg appearance-none cursor-pointer" />
             <Tooltip>{rangeVal}</Tooltip>
         </div>
     );
