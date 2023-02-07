cf8936e9489a89342ffc20b51210645c4039d0df
Safin Ali <safin.ali.7205@gmail.com>
2023-02-08 01:15:46 +0600

    others device navigate path implemented

--- src/Page/Product-Items/ProductItems.js
+++ src/Page/Product-Items/ProductItems.js
@@ -23,7 +23,7 @@ const ProductItems = () => {
     // get location device params
     const device = location.split('/')[3];
 
-    // get current others device name
+    // get others device name without current device
     const othersDevices = (function () {
         const categoriesData = (name) => {
             return categories.categoriesData.filter(elm => {
@@ -44,6 +44,11 @@ const ProductItems = () => {
         else {
             return navigate(`/products/${brandName.toLowerCase()}/${location.split('/')[2]}`);
         };
+    };
+
+    // navigate device path
+    const navigateDevice = (deviceName) => {
+        return navigate(`/products/${deviceName.toLowerCase()}`);
     };
 
     // fetch product data
@@ -72,8 +77,6 @@ const ProductItems = () => {
         return setLayoutUnit({ sideBarWidth, headerHeight, mrBool });
     };
 
-    console.log(othersDevices)
-
     if (isLoading) return <p>wait</p>;
 
     const { products, relatedBrands, upComingProducts } = productsData;
@@ -83,16 +86,20 @@ const ProductItems = () => {
             {/* Left Bar */}
             <LeftBar top={layoutUnit?.headerHeight} width={layoutUnit && layoutUnit.sideBarWidth} className={`hidden md:block md:fixed`}>
 
-                <div className={`mt-2 text-center`}><h5 className={`font-medium`}>OTHERS BRAND</h5></div>
-
-                <div className={`max-h-[80%] border-b overflow-y-scroll`}>
-                    <ul className={`text-center`}>
-                        {relatedBrands?.map(item => <li onClick={() => navigatePath(item.brandName)} className={`border-y p-2 cursor-pointer my-2`} key={item._id}>{item.brandName}</li>)}
-                    </ul>
+                <div className={`h-[70%] border-b`}>
+                    <div className={`mt-2 text-center`}><h5 className={`font-medium`}>RELATED BRAND</h5></div>
+                    <div className={`overflow-y-auto`}>
+                        <ul className={`text-center uppercase`}>
+                            {relatedBrands?.map(item => <li onClick={() => navigatePath(item.brandName)} className={`border-y p-2 cursor-pointer my-2`} key={item._id}>{item.brandName}</li>)}
+                        </ul>
+                    </div>
                 </div>
 
-                <div>
+                <div className={`text-center my-2`}>
+                    <h5 className={`font-medium mb-2`}>Search by Price Range</h5>
+                    <div className={`bg-blue-100`}>
                     <RangeSliderFeild></RangeSliderFeild>
+                    </div>
                 </div>
             </LeftBar>
 
@@ -114,21 +121,21 @@ const ProductItems = () => {
                         <h5 className={`font-medium`}>UP COMING</h5>
                     </div>
 
-                    <div className={`overflow-y-scroll`}>
+                    <div className={`overflow-y-auto`}>
                         <ul className={`text-center`}>
                             {upComingProducts?.map(item => <li onClick={() => navigatePath(item.brandName)} className={`border-y capitalize p-2 cursor-pointer my-2`} key={item._id}>{item.brand.concat(' ', item.others.model)}</li>)}
                         </ul>
                     </div>
                 </div>
 
-                <div className={`mt-3 min-h-[50%] border-b`}>
-                    <div className={`mt-2 text-center`}>
-                        <h5 className={`font-medium`}>More Devices</h5>
+                <div className={`mt-3 min-h-[50%]`}>
+                    <div className={`mt-2 text-center border-b`}>
+                        <h5 className={`font-medium mb-3`}>More Devices</h5>
                     </div>
 
-                    <div className={`overflow-y-scroll`}>
+                    <div className={`overflow-y-auto my-2`}>
                         <ul className={`text-center`}>
-                            {othersDevices?.map(item => <li onClick={() => navigatePath(item.category)} className={`border-y capitalize p-2 cursor-pointer my-2`} key={item._id}>{item.category}</li>)}
+                            {othersDevices?.map(item => <li onClick={() => navigateDevice(item.category)} className={`border capitalize p-2 cursor-pointer my-2`} key={item._id}>{item.category}</li>)}
                         </ul>
                     </div>
                 </div>
